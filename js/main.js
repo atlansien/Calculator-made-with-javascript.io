931cf9ee4d381963c64325804a44cb11e127c5d7
atlansien <atlansien@yahoo.co.jp>
2019-03-17 09:15:43 +0900

    calculateの改善、オブジェクトによる判別法に変更

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-let symbol = "+";
+const SYMBOL_TYPE = {
+  ADD: "+",
+  SUB: `-`,
+  MUL: "*",
+  DIV: `/`,
+  EQU: "="
+};
+let symbol = SYMBOL_TYPE.ADD;
+
+const FLAG_TYPE = {
+  NUMBER: 0,
+  OPERATOR: 1
+};
+let flag = FLAG_TYPE.NUMBER;
+
 let total = "";
 let currentValue = "";
-let flag = 0; // 数字 = 0, 演算子 = 1
+
 
 const screen = document.getElementById("screen");
 
 // 数字を入力
 const inputValue = data => {
   if (currentValue.length <= 8) { // 入力できる最大文字数
-    flag = 0;
+    flag = FLAG_TYPE.NUMBER;
     currentValue += data;
     screen.textContent = currentValue;
   }
@@ -23,7 +37,7 @@ const inputDot = data => {
 };
 
 // プラスマイナスの反転
-const inverted = () => {
+const invert = () => {
   if (currentValue === "") { // 合計の数字を反転
     total = -total;
     screen.textContent = total;
@@ -34,60 +48,53 @@ const inverted = () => {
 };
 
 // 計算をする
-const calclation = data => {
-  if (flag === 0 && data !== "=") { // =以外の記号を押した
-    flag = 1;
-
-    let formula = total + symbol + currentValue;
-    total = eval(formula);
-
+const calculate = data => {
+  if (flag === FLAG_TYPE.OPERATOR && data !== SYMBOL_TYPE.EQU) { // =を押した後数字を入力せず記号を押した
     symbol = data;
-    currentValue = "";
-    screen.textContent = total;
-
-  } else if (flag === 1 && data === "=") { // =を２回以上連打した
-    let formula = total + symbol + total;
-    total = eval(formula);
-
-    currentValue = "";
-    screen.textContent = total;
-
-  } else if (data === "=") { // =を一回押した
-    flag = 1;
-
-    let formula = total + symbol + currentValue;
-    total = eval(formula);
-
-    currentValue = "";
-    screen.textContent = total;
-
-  } else { // =を押した後数字を入力せず記号を押した
-    symbol = data;
+    return;
   }
+
+  let formula;
+  if (flag === FLAG_TYPE.NUMBER && data !== SYMBOL_TYPE.EQU) { // =以外の記号を押した
+    flag = FLAG_TYPE.OPERATOR;
+    formula = total + symbol + currentValue;
+    symbol = data;
+  } else if (flag === FLAG_TYPE.OPERATOR && data === SYMBOL_TYPE.EQU) { // =を２回以上連打した
+    if (total < 0) {
+      formula = total + symbol + -total;
+    } else {
+      formula = total + symbol + total;
+    }
+  } else if (data === SYMBOL_TYPE.EQU) { // =を一回押した
+    flag = FLAG_TYPE.OPERATOR;
+    formula = total + symbol + " " + currentValue; // " "はcurrentValueにマイナスの値が入っていた場合に必要
+  }
+
+  total = eval(formula);
+  currentValue = "";
+  screen.textContent = total;
 };
 
 const percent = () => {
-  if(symbol === "+" || symbol === "-") { // 足し算、引き算の場合の動作
-   let formula = currentValue / 100;
-   formula = total * formula;
-   currentValue = eval(formula);
-   
-   screen.textContent = currentValue;
- }else { // それ以外(数字のみ、掛け算、割り算の動作)
-   let formula = currentValue / 100;
-   currentValue = eval(formula);
+  if (symbol === SYMBOL_TYPE.ADD || symbol === SYMBOL_TYPE.SUB) { // 足し算、引き算の場合の動作
+    let formula = currentValue / 100;
+    formula = total * formula;
+    currentValue = eval(formula);
 
-   screen.textContent = currentValue;
- }
+    screen.textContent = currentValue;
+  } else { // それ以外(数字のみ、掛け算、割り算の動作)
+    let formula = currentValue / 100;
+    currentValue = eval(formula);
+
+    screen.textContent = currentValue;
+  }
 };
-
 
 // ACを押した場合の動作
-const allCrear = () => {
-  symbol = "+";
+const allClear = () => {
+  symbol = SYMBOL_TYPE.ADD;
   total = "";
   currentValue = "";
-  flag = 0;
+  flag = FLAG_TYPE.NUMBER;
   screen.textContent = "0";
 };
-
